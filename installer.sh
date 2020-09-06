1829011e7449794ad96570e1531674d818659d00
ozgurgundogan <ozgurgundogan90@gmail.com>
2020-09-06 20:36:33 +0300

    add installer

---
+++ installer.sh
@@ -0,0 +1,10 @@
+sudo apt install nodejs
+sudo apt install npm
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+nodejs -v
+node -v
+sudo npm install -g yarn
+sudo apt-get install python3-pip
+sudo apt-get install python3-venv
