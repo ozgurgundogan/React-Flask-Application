6c158859225200c2a6d3be1740c9ac77e8aa52a4
Özgür Gündoğan <ozgurgundogan90@gmail.com>
2020-09-06 21:35:20 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,10 +1,17 @@
-sudo apt install nodejs
-sudo apt install npm
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 nodejs -v
 node -v
 sudo npm install -g yarn
-sudo apt-get install python3-pip
-sudo apt-get install python3-venv
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python3-venv
+cd ./react-flask-app/
+yarn install --force
+cd ../api
+python3 -m venv react-flask-venv
+source react-flask-venv/bin/activate
+pip3 install -y -r requirements.txt
+deactivate
